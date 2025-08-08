a346e3ae387f989330e0d990d87b0770cb9fe976
yaminibandi <33190226+yaminibandi@users.noreply.github.com>
2025-08-08 22:44:30 +0530

    Add files via upload

---
+++ src/components/Todo.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function Todo() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo("");
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      {todos.map((item) => (
+        <h3>{item}</h3>
+      ))}
+    </div>
+  );
+}
